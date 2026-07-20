2695d53e9e0aeaca855518fde8208b30892e6662
zrangonazaire <61186442+zrangonazaire@users.noreply.github.com>
2026-07-20 13:50:45 +0000

    Coorection des FNE

--- gestimowebfront-end/src/environments/environment.prod.ts
+++ gestimowebfront-end/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // serverUrl: 'http://localhost:8287/actuator',
-  serverUrl: '/actuator',
+  serverUrl: 'http://localhost:8287/actuator',
   openAiApiKey: '',
 };
